821b763aa08b202fce64af2d86ebfe396a2ad9c0
GlassYearOpen <129816613+GlassYearOpen@users.noreply.github.com>
2023-04-03 23:50:08 -0400

    Add files via upload

---
+++ blankpagejs/index.js
@@ -0,0 +1,15 @@
+var x = window.open('','_blank');
+var doc = x.document;
+var ifrSrc =''
+
+
+if(window.location.href.includes('?url=')){
+    ifrSrc=window.location.href.substring(window.location.href.indexOf("?url=") + 5)
+}
+
+
+var iframe = doc.createElement('iframe');
+iframe.src=ifrSrc
+iframe.style.height='100%'
+iframe.style.width='100%'
+doc.body.appendChild(iframe)
